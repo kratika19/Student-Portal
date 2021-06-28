5983e71e8219ac35e02f804f11dd7fe9f1f0eb48
KRATIKA MITTAL <56781757+kratika19@users.noreply.github.com>
2021-06-28 17:55:41 +0530

    added notes and model

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+# sagar810
+class Notes(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+
+    class Meta:
+        verbose_name_plural = 'All Notes'
+
+    def __str__(self):
+        return self.title
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Notes
+
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'dashboard/home.html')
+
+
+def notes(request):
+    all_notes = Notes.objects.filter(user=request.user)
+    return render(request, 'dashboard/notes.html', {
+        'all_notes': all_notes
+    })
