99880a195a0469db26f6df3b088f61f6c6127113
KRATIKA MITTAL <56781757+kratika19@users.noreply.github.com>
2021-06-29 15:56:14 +0530

    added dictionary feature

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
     path('todo', views.todo, name='todo'),
     path('update-todo/<int:pk>', views.update_todo, name='update_todo'),
     path('delete-todo/<int:pk>', views.delete_todo, name='delete_todo'),
-    path('books', views.books, name='books')
+    path('books', views.books, name='books'),
+    path('dictionary', views.dictionary, name='dictionary')
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -198,3 +198,40 @@ def books(request):
     return render(request, 'dashboard/books.html', {
         'form': book_form
     })
+
+
+def dictionary(request):
+    if request.method == 'POST':
+        dictionary_form = DashboardForm(request.POST)
+        text = request.POST['text']
+        url = "https://api.dictionaryapi.dev/api/v2/entries/en_US/" + text
+        r = requests.get(url)
+        answer = r.json()
+        print(answer)
+        try:
+            phonetics = answer[0]['phonetics'][0]['text']
+            audio = answer[0]['phonetics'][0]['audio']
+            definition = answer[0]['meanings'][0]['definitions'][0]['definition']
+            synonyms = answer[0]['meanings'][0]['definitions'][0].get('synonyms'),
+            example = answer[0]['meanings'][0]['definitions'][0]['example']
+            context = {
+                'form': dictionary_form,
+                'input': text,
+                'phonetics': phonetics,
+                'audio': audio,
+                'definitions': definition,
+                'synonyms': synonyms,
+                'example': example,
+            }
+        except:
+            context = {
+                'form': dictionary_form,
+                'input': '',
+            }
+        return render(request, 'dashboard/dictionary.html', context)
+    else:
+        dictionary_form = DashboardForm()
+        context = {
+            'form': dictionary_form
+        }
+    return render(request, 'dashboard/dictionary.html', context)
